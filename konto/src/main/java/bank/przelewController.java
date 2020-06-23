40257891b9cc3704baeee72e2406315545d84edc
julnifares <56411219+julnifares@users.noreply.github.com>
2020-06-23 12:07:21 +0200

    Update przelewController.java

--- konto/src/main/java/bank/przelewController.java
+++ konto/src/main/java/bank/przelewController.java
@@ -5,11 +5,14 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+
 
 @Controller
 public class przelewController {
 	Konto inst = Konto.getInstance();
 	@RequestMapping("/przelewOK")
+	@PutMapping
 	    public String savePrzelew(@ModelAttribute Przelew przelew, BindingResult errors, Model model) {
 		
 			
